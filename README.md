941847d045abb8b215de755df95c32340e8b782f
Даша <dasha@MacBook-Air-Dasa.local>
2026-05-08 00:03:20 -0700

    Add project skeleton

---
+++ README.md
@@ -0,0 +1,12 @@
+# Dodge Game
+
+This is a simple console game written in C++.
+
+The project is built with CMake.
+
+Build:
+cmake -B build
+make -C build
+
+Run:
+./build/DodgeGame
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Dodge Game started!" << endl;
+    cout << "This is my laboratory project." << endl;
+
+    return 0;
+}
